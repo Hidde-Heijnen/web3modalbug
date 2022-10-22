3e27617a2c93801e3a0876f7b9ec7f7849d4c1ca
Hidde Heijnen <h.r.a.heijnen@students.uu.nl>
2022-10-22 12:38:44 +0200

    web3modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ConnectButton, useAccount, useBalance } from "@web3modal/react";
 
 function App() {
+  const { isConnected } = useAccount();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isConnected ? <Balances /> : <ConnectButton />}
       </header>
     </div>
   );
 }
 
 export default App;
+
+const Balances = () => {
+  const { address } = useAccount();
+  const [ethBal, setEthBal] = useState<string>("");
+  const [wethBal, setWethBal] = useState<string>("");
+
+  const { data, isLoading, error } = useBalance({
+    addressOrName: "0xed45ff9490723c2fb4a354e4b554c357604ea73c",
+    formatUnits: "ether",
+  });
+
+  const {
+    data: wethData,
+    isLoading: wethIsLoading,
+    error: wethError,
+  } = useBalance({
+    addressOrName: "0xed45ff9490723c2fb4a354e4b554c357604ea73c",
+    formatUnits: "ether",
+    token: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+  });
+
+  useEffect(() => {
+    if (!isLoading) {
+      setEthBal(parseFloat(data?.formatted!).toFixed(2));
+    }
+    if (!wethIsLoading) {
+      setWethBal(parseFloat(wethData?.formatted!).toFixed(2));
+    }
+  }, [address, isLoading, wethIsLoading]);
+  return (
+    <>
+      <span>Eth:{isLoading ? "Loading..." : ethBal}</span>
+      <span>Weth:{wethIsLoading ? "Loading..." : wethBal}</span>
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import type { ConfigOptions } from "@web3modal/react";
+import { Web3Modal } from "@web3modal/react";
+
+const config: ConfigOptions = {
+  projectId: "6ff1b53fe47d79442b512fd9a01b449b",
+  theme: "dark",
+  accentColor: "default",
+  ethereum: {
+    appName: "web3Modal",
+  },
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
+    <Web3Modal config={config} />
   </React.StrictMode>
 );
 
